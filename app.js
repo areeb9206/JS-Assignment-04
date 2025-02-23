3c80b43e6a9611ec5905f8c8b3630e3bf3f01312
areebahmed200801@gmail.com <areebahmed200801@gmail.com>
2025-02-24 01:39:46 +0500

    JS Assignment 04

--- app.js
+++ app.js
@@ -1,87 +1,87 @@
-// // Chapter 14-16:- ARRAYS
-// // Q1. Declare an empty array using JS literal notation to store student names in future.
-// var empArr = [];
+// Chapter 14-16:- ARRAYS
+// Q1. Declare an empty array using JS literal notation to store student names in future.
+var empArr = [];
 
-// // Q2. Declare an empty array using JS object notation to store student names in future
+// Q2. Declare an empty array using JS object notation to store student names in future
 
-// // Q3. Declare and initialize a strings array.
+// Q3. Declare and initialize a strings array.
 
-// var stringArr = ["Areeb", "Ahmed", "Ayan", "Faraz", "Waris"];
+var stringArr = ["Areeb", "Ahmed", "Ayan", "Faraz", "Waris"];
 
-// // Q4. Declare and initialize a numbers array
+// Q4. Declare and initialize a numbers array
 
-// var numArr = [10, 5, 15, 20, 30, 40, 50];
+var numArr = [10, 5, 15, 20, 30, 40, 50];
 
-// // Q5. Declare and initialize a boolean array.
-// var booleanArr = [true, false];
+// Q5. Declare and initialize a boolean array.
+var booleanArr = [true, false];
 
-// // Q6. Declare and initialize a mixed array.
-// var mixedArr = ["Areeb", 10, 5, true, false, null, undefined];
+// Q6. Declare and initialize a mixed array.
+var mixedArr = ["Areeb", 10, 5, true, false, null, undefined];
 
-// // Q7. Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like:
+// Q7. Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listed qualifications in your browser like:
 
-// var eduInKhi = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
-// document.write();
-// document.write(
-//   "<h2>Qualifications:-</h2>",
-//   " 1) ",
-//   eduInKhi[0],
-//   "<br>",
-//   " 2) ",
-//   eduInKhi[1],
-//   "<br>",
-//   " 3) ",
-//   eduInKhi[2],
-//   "<br>",
-//   " 4) ",
-//   eduInKhi[3],
-//   "<br>",
-//   " 5) ",
-//   eduInKhi[4],
-//   "<br>",
-//   " 6) ",
-//   eduInKhi[5],
-//   "<br>",
-//   " 7) ",
-//   eduInKhi[6],
-//   "<br>",
-//   " 8) ",
-//   eduInKhi[7],
-//   "<br><br>"
-// );
+var eduInKhi = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+document.write();
+document.write(
+  "<h2>Qualifications:-</h2>",
+  " 1) ",
+  eduInKhi[0],
+  "<br>",
+  " 2) ",
+  eduInKhi[1],
+  "<br>",
+  " 3) ",
+  eduInKhi[2],
+  "<br>",
+  " 4) ",
+  eduInKhi[3],
+  "<br>",
+  " 5) ",
+  eduInKhi[4],
+  "<br>",
+  " 6) ",
+  eduInKhi[5],
+  "<br>",
+  " 7) ",
+  eduInKhi[6],
+  "<br>",
+  " 8) ",
+  eduInKhi[7],
+  "<br><br>"
+);
 
-// // Q8. Write a program to store 3 student names in an array.Take another array to store score of these three students. Assume that total marks are 500 for each student, display the scores & percentages of students like:
+// Q8. Write a program to store 3 student names in an array.Take another array to store score of these three students. Assume that total marks are 500 for each student, display the scores & percentages of students like:
 
-// var studentName = ["Michael", "John", "Tony"];
-// var studentsScore = [320, 230, 450];
-// var percentage1 = (studentsScore[0] / 500) * 100;
-// var percentage2 = (studentsScore[1] / 500) * 100;
-// var percentage3 = (studentsScore[2] / 500) * 100;
+var studentName = ["Michael", "John", "Tony"];
+var studentsScore = [320, 230, 450];
+var percentage1 = (studentsScore[0] / 500) * 100;
+var percentage2 = (studentsScore[1] / 500) * 100;
+var percentage3 = (studentsScore[2] / 500) * 100;
 
-// document.write(
-//   "Score of ",
-//   studentName[0],
-//   " is ",
-//   studentsScore[0],
-//   ". Percentage : ",
-//   percentage1,
-//   "<br>",
-//   "Score of ",
-//   studentName[1],
-//   " is ",
-//   studentsScore[1],
-//   ". Percentage : ",
-//   percentage2,
-//   "<br>",
-//   "Score of ",
-//   studentName[2],
-//   " is ",
-//   studentsScore[2],
-//   ". Percentage : ",
-//   percentage3,"<br><br>"
-// );
+document.write(
+  "Score of ",
+  studentName[0],
+  " is ",
+  studentsScore[0],
+  ". Percentage : ",
+  percentage1,
+  "<br>",
+  "Score of ",
+  studentName[1],
+  " is ",
+  studentsScore[1],
+  ". Percentage : ",
+  percentage2,
+  "<br>",
+  "Score of ",
+  studentName[2],
+  " is ",
+  studentsScore[2],
+  ". Percentage : ",
+  percentage3,"<br><br>"
+);
 
-// // Q9. Initialize an array with color names. Display the array elements in your browser.
+// Q9. Initialize an array with color names. Display the array elements in your browser.
 
 var colorNames = [" Blue ", " Black ", " Green ", " Red " , " Yellow "];
 document.write(colorNames)
@@ -129,11 +129,12 @@ document.write("<br><br>", colorNames);
 // f. Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index. . 
 // Display the updated array in your browser.
 
-var index = splice() ;
+var index = +prompt("Enter Index number where you want to add the colors");
+var addColors = prompt("Enter Color names you want to add")
 
+colorNames.splice(index , 0 , addColors)
 
-
-
+document.write("<br><br>",colorNames)
 
 
 
